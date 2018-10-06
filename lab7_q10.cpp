71f7058a057d367891b31fccba3988d8c75761d5
fidasalim <42141530+fidasalim@users.noreply.github.com>
2018-10-06 23:52:29 +0530

    Add files via upload

---
+++ lab7_q10.cpp
@@ -0,0 +1,27 @@
+//including library
+#include <iostream>
+using namespace std;
+int fib(int n){
+	//conditions
+	if(n==1){
+	return 0;}
+	if(n==2){
+	return 1;}
+	//m the tern of fibonacci sequence
+	else{
+	return fib(n-1)+fib(n-2);}
+}
+
+
+int main(){
+	int a;
+	//asking a number
+	cout<<"Which term in Fibonacci sequence do you want?";
+	cout<<endl;
+	cin>>a;
+	cout<<endl;
+	//call for function
+	cout<<"The "<<a<<"th term of Fibenocci sequence is "<<fib(a)<<endl;
+return 0;
+
+}
