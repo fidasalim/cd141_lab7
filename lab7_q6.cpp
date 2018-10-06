6315847b38b74d93b94e83683c2f73a74a08bf36
fidasalim <42141530+fidasalim@users.noreply.github.com>
2018-10-06 23:10:38 +0530

    Update lab7_q6.cpp

--- lab7_q6.cpp
+++ lab7_q6.cpp
@@ -1,21 +1,27 @@
-//including library
+/including library
 #include <iostream>
 using namespace std;
-int reverse(int n){
+int reverse(int n,int m){
 	//conditions
 	if(n==0){
-	return 0;}
-	//reversing the number
+	return m;}
+	//storing the reverse of the number
 	else{
-	cout<<n%10;
-	return reverse(n/10);}
+	m=(m*10)+(n%10);
+	return reverse(n/10,m);}}
 
-}
 int main(){
 	int n;
 	//asking a number
 	cout<<"type a number : ";cin>>n;
-	reverse(n);cout<<endl;
+	int m = reverse(n,0);cout<<endl;
+	//printing the condition
+	if(m==n){
+	cout<<"The number "<<n<<" is palindromic"<<endl;
+}
+	else{
+	cout<<"The number "<<n<< " is not palindromic"<<endl;
+}
 return 0;
 
 }
